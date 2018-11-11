88b0f2807ee29bef6c4b8eb92a7be484c65d52ff
mr6r4y <mr6r4y@gmail.com>
2018-11-11 18:10:52 +0200

    Add argument for http auth

--- snapper.py
+++ snapper.py
@@ -37,6 +37,7 @@ def get_args():
     parser.add_argument("-d", "--dir", help="Directory to save the snapshots")
     parser.add_argument("-j", "--json-file", help="JSON file with the list of domains to snap")
     parser.add_argument("-p", "--uri-path", default="", help="Add path to every URI")
+    parser.add_argument("-t", "--httpauth-creds", help="Set user and password for HTTP Authenticated pages")
 
     args = parser.parse_args()
 
@@ -49,10 +50,12 @@ def main():
     save_dir = os.path.abspath(args.dir)
     domains = json.load(open(args.json_file, "r"))
     uri_path = args.uri_path
+    httpauth_creds = args.httpauth_creds.split("::") if args.httpauth_creds else None
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+    chrome_options.add_argument("--ignore-certificate-errors")
     chrome_options.binary_location = CHROME_PATH
 
     driver = webdriver.Chrome(
@@ -60,15 +63,24 @@ def main():
         chrome_options=chrome_options
     )
 
+    if httpauth_creds:
+        u, p = httpauth_creds
+        u_http_ptrn = "http://" + u + ":" + p + "@%s/%s"
+        u_https_ptrn = "https://" + u + ":" + p + "@%s/%s"
+    else:
+        u_http_ptrn = "http://%s/%s"
+        u_https_ptrn = "https://%s/%s"
+
     for d in domains:
-        u_http = "http://%s/%s" % (d, uri_path)
+        u_http = u_http_ptrn % (d, uri_path)
         f_http = os.path.join(save_dir, "http_%s.png" % d)
-        u_https = "https://%s/%s" % (d, uri_path)
+        u_https = u_https_ptrn % (d, uri_path)
         f_https = os.path.join(save_dir, "https_%s.png" % d)
 
         log.info("Save %s" % f_http)
         make_screenshot(driver, u_http, f_http)
         log.info("Save %s" % f_https)
+        log.info("uri: %s" % u_https)
         make_screenshot(driver, u_https, f_https)
 
     driver.close()
